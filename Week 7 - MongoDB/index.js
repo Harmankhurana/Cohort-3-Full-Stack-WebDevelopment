0be7c677a3a8480274cb6e0fbecfd04aff8aaf0b
Harman khurana <work.harmankhurana@gmail.com>
2026-02-13 08:06:56 +0530

    Creating the Backend of todo app and using mongoDB for DB

--- Week 7 - MongoDB/index.js
+++ Week 7 - MongoDB/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 // Mongoose is a MongoDB object modeling tool designed to work in an asynchronous environment
-import mongoose from "mongoose";
+import mongoose, { ConnectionStates } from "mongoose";
+import dotenv, { config, configDotenv } from 'dotenv';
 
 const app = express();
 app.use(express.json());
+dotenv.config();
+const PORT = process.env.PORT;
 
 app.post("/signup" , function (req, res) {
 
@@ -20,3 +23,7 @@ app.post("/todo" , function (req, res) {
 app.get("/todo" , function (req, res) {
 
 });
+
+app.listen(PORT, () => {
+    console.log(`Server is running`);
+})
