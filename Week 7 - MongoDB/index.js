2254ad56a01cf60a0c53877f970dab2c5e1ebac3
Harman khurana <work.harmankhurana@gmail.com>
2026-02-13 07:17:00 +0530

    Creating the Backend of todo app and using mongoDB for DB

--- Week 7 - MongoDB/index.js
+++ Week 7 - MongoDB/index.js
@@ -3,3 +3,19 @@ import mongoose from "mongoose";
 
 const app = express();
 app.use(express.json());
+
+app.post("/signup" , function (req, res) {
+
+});
+
+app.post("/login" , function (req, res) {
+
+});
+
+app.post("/todo" , function (req, res) {
+
+});
+
+app.get("/todo" , function (req, res) {
+
+});
