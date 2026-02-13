95a52702b78eeb4d5c0e9409641764b91a4386ab
Harman khurana <work.harmankhurana@gmail.com>
2026-02-13 09:14:59 +0530

    Creating the Backend of todo app and using mongoDB for DB

--- Week 7 - MongoDB/db.js
+++ Week 7 - MongoDB/db.js
@@ -1,1 +1,16 @@
 import mongoose from "mongoose";
+
+const Schema = mongoose.schema;
+const objectId = mongoose.objectId;
+
+const User = new Schema({
+    name: String,
+    email: String,
+    password: String,
+})
+
+const Todo = new schema({
+    userId: objectId,
+    title: String,
+    done: Boolean,
+})
