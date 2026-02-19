593dc0cb4eb3e4e9d5e19fbe9726ce03e6f54e7b
Harman khurana <work.harmankhurana@gmail.com>
2026-02-20 04:04:58 +0530

    Writing skeleton code for admin.js and importing it in index.js file

---
+++ Week 8 - Course Selling App/routes/admin.js
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+const adminRouter = Router();
+
+adminRouter.post('/signup', function(req, res) {
+
+});
+
+adminRouter.post('/signin', function(req, res) {
+    
+});
+
+adminRouter.post('/course', function(req, res) {
+    
+});
+
+adminRouter.put('/course', function(req, res) {
+    
+});
+
+adminRouter.get('/course', function(req, res) {
+    
+});
+
+export { adminRouter };
