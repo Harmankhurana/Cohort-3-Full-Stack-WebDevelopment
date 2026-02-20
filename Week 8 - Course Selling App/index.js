e81c373ecc2680c8071e1b3f270227aa784452c5
Harman khurana <work.harmankhurana@gmail.com>
2026-02-20 13:59:00 +0530

    Only starting the server when DB is connected first

--- Week 8 - Course Selling App/index.js
+++ Week 8 - Course Selling App/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import { userRouter } from './routes/user.js';
 import { courseRouter } from './routes/course.js';
 import { adminRouter } from './routes/admin.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const DB = process.env.DB;
 
 app.use(express.json());
 
@@ -12,6 +14,15 @@ app.use('/api/v1/user', userRouter);
 app.use('/api/v1/admin', adminRouter);
 app.use('/api/v1/course', courseRouter);
 
-app.listen(PORT, () => {
-    console.log(`Server is running`);
-});
+
+// connecting the Database first and then running the server
+function connectionFirst() {
+    mongoose.connect(DB);
+    app.listen(PORT, () => {
+        console.log(`Server is running`);
+    });    
+};
+
+// calling the function here
+connectionFirst();
+
