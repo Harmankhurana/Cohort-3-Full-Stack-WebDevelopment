a3f329c13245c739932d2fcb878ec7e50d5964a8
Harman khurana <work.harmankhurana@gmail.com>
2026-02-21 12:25:34 +0530

    Coding the main business logic in admin.js

--- Week 8 - Course Selling App/routes/admin.js
+++ Week 8 - Course Selling App/routes/admin.js
@@ -1,12 +1,65 @@
 import { Router } from 'express';
+import { bcrypt } from 'bcrypt';
+import { jwt } from 'jsonwebtoken';
+import { z } from 'zod';
 const adminRouter = Router();
 import { AdminModel } from '../db.js';
+import { parse } from 'dotenv';
 
-adminRouter.post('/signup', function(req, res) {
+const saltRounds = 10;
+const JWT_SECRET = USER_APP;
 
+adminRouter.post('/signup', async function(req, res) {
+    const requiredBody = z.object({
+        name: z.string(),
+        email: z.string(),
+        password: z.string(),
+    });
+
+    const parseDataWithSuccess = requiredBody.safeParse(req.body);
+
+    if(!parseDataWithSuccess.success){
+        res.json({
+            message: "Incorrect format",
+            error: parseDataWithSuccess,
+        })
+    };
+
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try {
+    const hashedPassword = bcrypt.hash(password, saltRounds);
+    console.log(hashedPassword);
+
+    await AdminModel.create({
+        name: name,
+        email: email,
+        password: hashedPassword,
+    });
+    res.json({
+        message: "You're signedup to admin page"
+    })
+    } catch (e) {
+        res.json({
+            message: "Something went wrong while signing up"
+        })
+    }
 });
 
 adminRouter.post('/signin', function(req, res) {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const response = bcrypt.compare(password, hashedPassword);
+    if(!response){
+        res.json({
+            message: "Incorrect credentials",
+        });
+    }
+
     
 });
 
