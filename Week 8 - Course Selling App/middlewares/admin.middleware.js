4941feafb5859620f55d62d7ad8d5870e40eeebe
Harman khurana <work.harmankhurana@gmail.com>
2026-02-24 00:22:02 +0530

    Making middlewares for user and admin page

--- Week 8 - Course Selling App/middlewares/admin.middleware.js
+++ Week 8 - Course Selling App/middlewares/admin.middleware.js
@@ -1,5 +1,14 @@
+import { jwt } from 'jsonwebtoken';
+
 function adminMiddleware(req, res, next) {
+    const token = req.headers.token;
+
+    const response = jwt.verify({
+        token,
+    },JWT_ADMIN_PASSWORD)
 
 };
 
-export { adminMiddleware };
+export { 
+    adminMiddleware,
+ };
