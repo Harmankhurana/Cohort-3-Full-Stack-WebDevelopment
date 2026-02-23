3c0cf64c8c5c24c9131225567ac812feafb13be1
Harman khurana <work.harmankhurana@gmail.com>
2026-02-24 00:37:09 +0530

    Making user and admin auth middleware to verify

--- Week 8 - Course Selling App/middlewares/admin.middleware.js
+++ Week 8 - Course Selling App/middlewares/admin.middleware.js
@@ -1,12 +1,21 @@
 import { jwt } from 'jsonwebtoken';
+import { JWT_ADMIN_PASSWORD } from '../config.js';
 
 function adminMiddleware(req, res, next) {
     const token = req.headers.token;
 
     const response = jwt.verify({
         token,
-    },JWT_ADMIN_PASSWORD)
+    },JWT_ADMIN_PASSWORD);
 
+    if(response) {
+        req.userId = response.id;
+        next();
+    } else {
+        res.json({
+            message: "You're not signed in"
+        })
+    }
 };
 
 export { 
--- Week 8 - Course Selling App/middlewares/user.middleware.js
+++ Week 8 - Course Selling App/middlewares/user.middleware.js
@@ -1,5 +1,23 @@
+import { jwt } from 'jsonwebtoken';
+import { JWT_USER_PASSWORD } from "../config.js";
+
 function userMiddleware(req, res, next) {
-    
+    const token = req.headers.token;
+
+    const response = jwt.verify({
+        token,
+    }, JWT_USER_PASSWORD);
+
+    if(response){
+        req.userId = response.id;
+        next();
+    } else {
+        res.json({
+            message: "You're not signed in"
+        })
+    }
 };
 
-export { userMiddleware };
+export { 
+    userMiddleware,
+};
