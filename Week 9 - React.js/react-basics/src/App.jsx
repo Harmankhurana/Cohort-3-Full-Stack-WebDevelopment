5bad2ec783605943e4d1d618d191b052f02324d4
Harman khurana <work.harmankhurana@gmail.com>
2026-03-24 22:09:13 +0530

    Basic react code for changing the state of counter button

--- Week 9 - React.js/react-basics/src/App.jsx
+++ Week 9 - React.js/react-basics/src/App.jsx
@@ -1,9 +1,17 @@
+// useState is a Hook that lets you add state to functional components. It returns an array with the current state and a function to update it
+import { useState } from "react"
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  // function for changing the count state
+  function counter() {
+    setCount(count + 1);
+  }
 
   return (
     <>
-      
+      <button onClick={counter}>Add value: {count}</button>
     </>
   )
 }
