33d74f4ee342b6daef9015de626a5b01ad8a5a5a
Harman khurana <work.harmankhurana@gmail.com>
2026-03-26 01:53:05 +0530

    Creating conditional rendering component for counter

--- Week 9 - React.js/react-basics/src/ConditionalRendering.jsx
+++ Week 9 - React.js/react-basics/src/ConditionalRendering.jsx
@@ -5,18 +5,28 @@ import { useEffect, useState } from "react";
 
 
 function ConditionalRendering() {
-    let counterVisible = true;
+    const [counterVisible, setCounterVisible] = useState(true);
+
+    // useEffect for flipping the value of counterVisible(true/false) after every 5 sec
+    useEffect(function() {
+        const counterInterval = setInterval(function() {
+            setCounterVisible(counterVisible => !counterVisible);
+        }, 5000)
+
+        return () => clearInterval(counterInterval);
+    }, []);
 
     return (
         <div>
-            {counterVisible ? <Counter /> : null}
+            {/* {counterVisible ? <Counter /> : null}  */}
+            {counterVisible && <Counter />}
         </div>
     )
+}
 
     function Counter() {
 
         const [count, setCount] = useState(0);
-
         console.log("Counter");
 
         useEffect(function() {
@@ -31,6 +41,6 @@ function ConditionalRendering() {
             <h1>{count}</h1>
         )
     }
-}
+
 
 export default ConditionalRendering
