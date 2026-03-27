ced80d2846e3b63fada637a7085e047ce94a6dd9
Harman khurana <work.harmankhurana@gmail.com>
2026-03-27 14:04:38 +0530

    feat(Props): Creating a conditionally rendered time and clock

--- Week 9 - React.js/react-basics/src/Props.jsx
+++ Week 9 - React.js/react-basics/src/Props.jsx
@@ -15,7 +15,7 @@ function App() {
                         {/* Call PostComponent here with props to render it in the App component */}
                         <PostComponent
                             name={"Harman"}
-                            followerCount={20}
+                            subtitle={"20 followers"}
                             time={"2m ago"}
                             image={"https://images.ctfassets.net/ub3bwfd53mwy/5zi8myLobtihb1cWl3tj8L/45a40e66765f26beddf7eeee29f74723/6_Image.jpg?w=750"}
                             description={"What to know how to win big? Check out how these folks won $6000 in bounties."}
@@ -26,7 +26,7 @@ function App() {
                             {/* Call PostComponent here with props to render it in the App component */}
                             <PostComponent
                                 name={"Harkirat"}
-                                followerCount={30}
+                                subtitle={"Promotion"}
                                 time={"3m ago"}
                                 image={"https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"}
                                 description={"How to get hired in 2024? I lost my Job in 2023, this is the roadmap I followed to get hired in 2024."}
@@ -38,7 +38,7 @@ function App() {
                             {/* Call PostComponent here with props to render it in the App component */}
                             <PostComponent
                                 name={"Simar"}
-                                followerCount={50}
+                                subtitle={"50 followers"}
                                 time={"18m ago"}
                                 image={"https://imgs.search.brave.com/ZACv93qZO57A2RrexnAjJi9CTpejuyu2aIGeB9-2beA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJhY2Nlc3Mu/Y29tL2Z1bGwvMTk0/OTM2My5qcGc"}
                                 description={"Here is the roadmap to become a full-stack developer in 2024."}
@@ -63,21 +63,23 @@ const style = {
 };
 
 // Create a function component named PostComponent with props to render it in the App component
-function PostComponent({ name, followerCount, time, image, description }) {
+function PostComponent({ name, subtitle, time, image, description }) {
     // return JSX that will be rendered
     return (
         // Apply style object to the div element
         <div style={style}>
-            {/* display the name, followerCount, time, image, and description using props */}
+            {/* display the name, subtitle, time, image, and description using props */}
             <div style={{ display: "flex" }}>
                 <img src={image} style={{ width: 40, height: 40, borderRadius: 40 }} />
                 <div style={{ fontSize: 14, marginLeft: 10 }}>
                     <b>{name}</b> 
-                    <div>{followerCount} followers</div>
-                    <div style={{display : "flex"}}>
+                    <div>{subtitle}</div>
+
+                    {/* I want this time and clock to be conditionally rendered, that's why putting it in curly braces {} */}
+                    { <div style={{display : "flex"}}>
                         <div>{time}</div>
                         <img src="https://www.pngfind.com/pngs/m/589-5899814_png-file-svg-clock-icon-transparent-png.png" alt="clock timer" style={{width : 10, height : 10, paddingLeft : 5, marginTop : 2}}/>                        
-                    </div>
+                    </div> }
 
                 </div>
             </div>
