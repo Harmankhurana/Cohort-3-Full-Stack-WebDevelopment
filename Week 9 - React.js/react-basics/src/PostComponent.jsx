96e0f3769f042752f33df524a5072ddca5fbfe6b
Harman khurana <work.harmankhurana@gmail.com>
2026-03-27 10:13:02 +0530

    feat(Post Component): Adding logo image for the Post

--- Week 9 - React.js/react-basics/src/PostComponent.jsx
+++ Week 9 - React.js/react-basics/src/PostComponent.jsx
@@ -1,11 +1,24 @@
 function PostComponent() {
+    return(
+        <div>
+            <Post />
+        </div>
+    )
+}
+
+const style = {
+    width : 200, 
+    backgroundColor : "white",
+    borderRadius : 10,
+    borderColor : "grey",
+    borderWidth : 1,
 
 }
 
 function Post() {
     return (
-        <div>
-            
+        <div style={style}>
+            <img src={"https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"} alt="Logo image" style={{width : 20, height : 20, borderRadius : 20}}/>
         </div>
     )
 }
