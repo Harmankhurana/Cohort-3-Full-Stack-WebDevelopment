114d200d3b247d241c3fbe0527b4b37e4357066f
Harman khurana <work.harmankhurana@gmail.com>
2026-03-27 10:32:06 +0530

    feat(Post Component): Creating a static Post Component

--- Week 9 - React.js/react-basics/src/PostComponent.jsx
+++ Week 9 - React.js/react-basics/src/PostComponent.jsx
@@ -1,6 +1,6 @@
 function PostComponent() {
     return(
-        <div>
+        <div style={{background : "#dfe6e9", height : "100vh"}}>
             <Post />
         </div>
     )
@@ -12,14 +12,26 @@ const style = {
     borderRadius : 10,
     borderColor : "grey",
     borderWidth : 1,
-
 }
 
 function Post() {
     return (
         <div style={style}>
-            <img src={"https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"} alt="Logo image" style={{width : 20, height : 20, borderRadius : 20}}/>
+            <div style={{display : "flex"}}>
+                <img src={"https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"} alt="Logo image" style={{width : 30, height : 30, borderRadius : 20}}/>
+                <div style={{fontSize : 10, marginLeft : 10}}>
+                    <b>
+                       100xDevs 
+                    </b>
+                    <div>23,888 followers</div>
+                    <div>12m</div>
+                </div>
+            </div>
+            <div style={{fontSize : 12}}>
+                What to know to win big? Check out how these folks won $6000 in bounties.
+            </div>          
         </div>
+
     )
 }
 
