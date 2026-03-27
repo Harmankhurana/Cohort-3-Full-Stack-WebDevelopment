31f9dbee5b692f9712c1b707095f1c1f660717d5
Harman khurana <work.harmankhurana@gmail.com>
2026-03-28 03:56:11 +0530

    Writing the structure of the App.jsx that will render

--- Week 9 - React.js/react-advance/src/App.jsx
+++ Week 9 - React.js/react-advance/src/App.jsx
@@ -1,10 +1,22 @@
 import PostComponent from './Post.jsx'
+
 function App() {
   
+  function addPost() {
 
+  }
   return(
-    <div>
-      Post Application
+    <div style={{background : "#def6e9", height : "100vh"}}>
+      <button onClick={addPost}>Add Post</button>
+      <div style={{display : "flex", justifyContent : "center"}}>
+        <PostComponent 
+          name = {"Harman"}
+          subtitle = {"156k followers"}
+          time = {"2m ago"}
+          image = {"https://images.ctfassets.net/ub3bwfd53mwy/5zi8myLobtihb1cWl3tj8L/45a40e66765f26beddf7eeee29f74723/6_Image.jpg?w=750"}
+          description = {"What to know to win big? Check out how these folks won $6000 in bounties"}
+        />
+      </div>
     </div>
   )
 }
