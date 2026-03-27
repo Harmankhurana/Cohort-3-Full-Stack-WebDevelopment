0fec2a182e1f3449b8587d0f0d460b4c4a88c2f3
Harman khurana <work.harmankhurana@gmail.com>
2026-03-27 20:51:04 +0530

    feat(Toggle Message): Creating a toggle message component

---
+++ Week 9 - React.js/react-basics/src/ToggleMessage.jsx
@@ -0,0 +1,13 @@
+function App() {
+    return(
+        <div style={{background : "#def6e9", height : "100vh"}}>
+            <ToggleMessage />
+        </div>
+    )
+}
+
+function ToggleMessage() {
+
+}
+
+export default App
