ae6bc84bc0e13b08db6736edf3891f893dde1b33
Harman khurana <work.harmankhurana@gmail.com>
2026-04-01 16:50:28 +0530

    feat(App): Creating a In-memory array which keeps all the data of posts

--- Week 9 - React.js/react-advance/src/App.jsx
+++ Week 9 - React.js/react-advance/src/App.jsx
@@ -1,7 +1,15 @@
 import PostComponent from './Post.jsx'
 
 function App() {
-  
+    // In memory array for keeping the post
+    const posts = [{
+          name : "Harman",
+          subtitle : "156k followers",
+          time : "2m ago",
+          image : "https://images.ctfassets.net/ub3bwfd53mwy/5zi8myLobtihb1cWl3tj8L/45a40e66765f26beddf7eeee29f74723/6_Image.jpg?w=750",
+          description : "What to know to win big? Check out how these folks won $6000 in bounties",
+    },]
+
   function addPost() {
 
   }
