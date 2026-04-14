cd3ae301d977a4db425177c89616eaef46ec06d4
Harman khurana <work.harmankhurana@gmail.com>
2026-04-14 22:40:18 +0530

    feat: Adding more dynamic component in Card wrapper

--- Week 9 - React.js/react-basics/src/Card.jsx
+++ Week 9 - React.js/react-basics/src/Card.jsx
@@ -1,15 +1,30 @@
 // A card component where I'm implementing Children and Props - a common component that can be used for multiple purposes
 
 function Children() {
-    // Different innerContent
-    return <div>
-        <Card innerContent={"Hi there"} />
+    // Different innerContent - dynamic component
+    return <div style={{display : "flex"}}>
+
+    {/* first innerContent */}
+        <Card innerContent = {"Hi there"} />
+
+    {/* Second innerContent */}
+        <Card innerContent = {<div style={{color : "green"}}>
+                "Hi there"
+            </ div>} />
+
+    {/* Third innerContent */}
+        <Card innerContent = {<div style = {{color :"green"}}>
+                "What You want to post"
+                <br /> <br />
+                <input type="text" />
+            </div>}/>
+
     </div>
     
-    // Common card component
+    // Common card component - static component
     function Card({innerContent}) {
         return (
-            <div style = {{background : "black", borderRadius : "10", color : "white", padding : 10, margin : 10}}>
+            <div style = {{background : "black", borderRadius : 10, color : "white", padding : 10, margin : 10}}>
                 {innerContent}
             </div>            
         )
