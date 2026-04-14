9ac6faef1edcc16bf875ce0ffa4ec5f02ee44d23
Harman khurana <work.harmankhurana@gmail.com>
2026-04-14 14:10:09 +0530

    feat: Creating a todo Application and using Fetch API

--- Week 9 - React.js/react-basics/src/FetchAPI.jsx
+++ Week 9 - React.js/react-basics/src/FetchAPI.jsx
@@ -1,15 +1,20 @@
 // Making a To-Do application and fetching data
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function TodoApplication() {
-    const [currentTab, setCurrentTab] = useState();
+    const [currentTab, setCurrentTab] = useState(1);
+    const [tabData, setTabData] = useState({});
+
+    useEffect(function() {
+        fetch('https://jsonplaceholder.typicode.com/todos/' + currentTab);
+    }, [currentTab])
 
     return(
         <div>
-            <Button style = {{color : currentTab === 1 ? "red" : "black"}}>Todo #1</Button>
-            <Button style = {{color : currentTab === 2 ? "red" : "black"}}>Todo #2</Button>
-            <Button style = {{color : currentTab === 3 ? "red" : "black"}}>Todo #3</Button>
-            <Button style = {{color : currentTab === 4 ? "red" : "black"}}>Todo #4</Button>
+            <Button onClick = {() => setCurrentTab(1)} style = {{color : currentTab === 1 ? "red" : "black"}}>Todo #1</Button>
+            <Button onClick = {() => setCurrentTab(2)} style = {{color : currentTab === 2 ? "red" : "black"}}>Todo #2</Button>
+            <Button onClick = {() => setCurrentTab(3)} style = {{color : currentTab === 3 ? "red" : "black"}}>Todo #3</Button>
+            <Button onClick = {() => setCurrentTab(4)} style = {{color : currentTab === 4 ? "red" : "black"}}>Todo #4</Button>
         </div>
     )
 }
