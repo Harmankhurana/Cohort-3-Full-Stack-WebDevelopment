79be763b6ce85b59c3710700615e36adea136f37
Harman khurana <work.harmankhurana@gmail.com>
2026-04-14 22:58:09 +0530

    feat: Chaning the name from innerContent to children

--- Week 9 - React.js/react-basics/src/Card.jsx
+++ Week 9 - React.js/react-basics/src/Card.jsx
@@ -1,35 +1,40 @@
-// A card component where I'm implementing Children and Props - a common component that can be used for multiple purposes
+// A card component where I'm implementing children and Props - a common component that can be used for multiple purposes
 
-function Children() {
-    // Different innerContent - dynamic component
+function ChildrenComponent() {
+    // Different children - dynamic component
     return <div style={{display : "flex"}}>
 
-    {/* first innerContent */}
-        <Card innerContent = {"Hi there"} />
+    {/* first children */}
+        <Card children = {"Hi there"} />
 
-    {/* Second innerContent */}
-        <Card innerContent = {<div style={{color : "green"}}>
+    {/* Second children */}
+        <Card children = {<div style={{color : "green"}}>
                 "Hi there"
             </ div>} />
 
-    {/* Third innerContent */}
-        <Card innerContent = {<div style = {{color :"green"}}>
+    {/* Third children */}
+        <Card children = {<div style = {{color :"green"}}>
                 "What You want to post"
                 <br /> <br />
                 <input type="text" />
             </div>}/>
 
+    {/* all these 3 dynamic components are good but this component is having much better syntax */}
+        <Card>
+            "Hi there"
+        </Card>
+
     </div>
     
     // Common card component - static component
-    function Card({innerContent}) {
+    function Card({children}) {
         return (
             <div style = {{background : "black", borderRadius : 10, color : "white", padding : 10, margin : 10}}>
-                {innerContent}
+                {children}
             </div>            
         )
 
     }
 }
 
-export default Children
+export default ChildrenComponent
