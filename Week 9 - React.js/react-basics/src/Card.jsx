57ca8bde8509babdb051fa05bcbc319b4b3310a2
Harman khurana <work.harmankhurana@gmail.com>
2026-04-16 04:44:49 +0530

    Making some spaces between the lines for better code readability

--- Week 9 - React.js/react-basics/src/Card.jsx
+++ Week 9 - React.js/react-basics/src/Card.jsx
@@ -2,39 +2,41 @@
 
 function ChildrenComponent() {
     // Different children - dynamic component
-    return <div style={{display : "flex"}}>
+    return (
+     <div style={{display : "flex"}}>
 
-    {/* first children */}
-        <Card children = {"Hi there"} />
-
-    {/* Second children */}
-        <Card children = {<div style={{color : "green"}}>
+        {/* first children */}
+            <Card children = {"Hi there"} />
+    
+        {/* Second children */}
+            <Card children = {<div style={{color : "green"}}>
+                    "Hi there"
+                </ div>} />
+    
+        {/* Third children */}
+            <Card children = {<div style = {{color :"green"}}>
+                    "What You want to post"
+                    <br /> <br />
+                    <input type="text" />
+                </div>}/>
+    
+        {/* all these 3 dynamic components are good but this component is having much better syntax */}
+            <Card>
                 "Hi there"
-            </ div>} />
-
-    {/* Third children */}
-        <Card children = {<div style = {{color :"green"}}>
-                "What You want to post"
-                <br /> <br />
-                <input type="text" />
-            </div>}/>
-
-    {/* all these 3 dynamic components are good but this component is having much better syntax */}
-        <Card>
-            "Hi there"
-        </Card>
-
-    {/* this is the same 3rd component but with a another syntax */ }
-        <Card>
-            {<div style = {{color :"green"}}>
-                "What You want to post"
-                <br /> <br />
-                <input type="text" />
-            </div>}
-        </Card>
+            </Card>
+    
+        {/* this is the same 3rd component but with a another syntax */ }
+            <Card>
+                {<div style = {{color :"green"}}>
+                    "What You want to post"
+                    <br /> <br />
+                    <input type="text" />
+                </div>}
+            </Card>
 
     </div>
-    
+);
+
     // Common card component - static component
     function Card({children}) {
         return (
