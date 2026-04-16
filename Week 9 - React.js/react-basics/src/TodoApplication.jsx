d3c2730861abf5c7e473fa8f2ee46053e9d85a91
Harman khurana <work.harmankhurana@gmail.com>
2026-04-17 01:22:04 +0530

    feat: Creating a Todo component and understanding Lists and Keys

--- Week 9 - React.js/react-basics/src/TodoApplication.jsx
+++ Week 9 - React.js/react-basics/src/TodoApplication.jsx
@@ -1,8 +1,28 @@
 // Creating a Todo Component and understanding Lists and Keys
 
-function ListsAndKeys() {
-    return(
-        <div>Lists and Keys</div>
+const ListsAndKeys = () => {
+    const todos = [{
+        title : "Go to Gym",
+        done : false,
+    }, {
+        title : "Eat food",
+        done : true,
+    }
+];
+
+    const todosComponent = todos.map(todo => < Todo title={todo.title} done={todo.done}/>)
+    return (
+        <div>
+            {todosComponent}
+        </div>
+    )
+};
+
+function Todo({title, done}) {
+    return (
+        <div>
+            {title} - {done ? "Done!" : "Not Done!"}
+        </div>
     )
 }
 
