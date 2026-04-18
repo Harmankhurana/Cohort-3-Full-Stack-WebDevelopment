66abf35c080f3e2c2f2dbcb7e315cbec255b0273
Harman khurana <work.harmankhurana@gmail.com>
2026-04-18 14:33:48 +0530

    feat: Creating a component to understand Inline styling in React

---
+++ Week 9 - React.js/react-basics/src/InlineStyling.jsx
@@ -0,0 +1,17 @@
+// Inline styling in React allows you to apply CSS styles directly to elements using a JavaScript object
+
+const InlineStylingComponent = () => {
+    return (
+        <div>
+            {InlineStyling}
+        </div>
+    );
+
+    function InlineStyling() {
+        return( 
+            <div></div>
+        )
+    };
+};
+
+export default InlineStylingComponent
