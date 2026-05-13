3e915cfd5080d5d15e2fc4014d6af52dcaf2756a
Harman khurana <work.harmankhurana@gmail.com>
2026-05-13 19:41:41 +0530

    feat: Writing the skeleton code for React Router

---
+++ Week 10 - React.js Advance/SPAs-and-Routing/src/Router.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// components from the react-router-dom library used for routing (navigation) in React apps.
+import {BrowserRouter, Router, Route} from 'react-router-dom';
+
+function ReactRouter() {
+    return (
+        <>
+        </>
+    )
+}
+
+export default ReactRouter
