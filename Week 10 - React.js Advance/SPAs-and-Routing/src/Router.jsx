f5f4e66a108119f8526438c612600202c41120dd
Harman khurana <work.harmankhurana@gmail.com>
2026-05-14 13:25:07 +0530

    feat: working on components from react-router-dom

--- Week 10 - React.js Advance/SPAs-and-Routing/src/Router.jsx
+++ Week 10 - React.js Advance/SPAs-and-Routing/src/Router.jsx
@@ -10,10 +10,25 @@ function ReactRouter() {
             <BrowserRouter>
                 {/* here i will define all my routes */}
                 <Routes>
-
+                    <Route path = "/neet/online-coaching-class-11" element = { <class11program /> } />
+                    <Route path = "/neet/online-coaching-class-12" element = { <class12program /> } />
                 </Routes>
             </BrowserRouter>
         </div>
+    )
+}
+
+// creating a function for class-11-program, calling it in ReactRouter (element)
+function class11program() {
+    return (
+        <div>NEET programs for 11th class</div>
+    )
+}
+
+// creating a function for class-12-program, calling it in ReactRouter (element)
+function class12program() {
+    return (
+        <div>NEET programs for 12th class</div>
     )
 }
 
