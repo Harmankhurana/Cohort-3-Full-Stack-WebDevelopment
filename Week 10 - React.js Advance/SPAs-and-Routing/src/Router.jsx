25bd90057126655207086ea83925f6d360310cb7
Harman khurana <work.harmankhurana@gmail.com>
2026-05-14 13:12:14 +0530

    feat: working on components from react-router-dom

--- Week 10 - React.js Advance/SPAs-and-Routing/src/Router.jsx
+++ Week 10 - React.js Advance/SPAs-and-Routing/src/Router.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
 // components from the react-router-dom library used for routing (navigation) in React apps.
-import {BrowserRouter, Router, Route} from 'react-router-dom';
+import {BrowserRouter, RouteS, Route} from 'react-router-dom';
 
 function ReactRouter() {
     return (
-        <>
-        </>
+        <div>
+            {/* making a BrowserRouter wrapper, we will wrap our application in BrowserRouter */}
+            <BrowserRouter>
+                {/* here i will define all my routes */}
+                <Routes>
+
+                </Routes>
+            </BrowserRouter>
+        </div>
     )
 }
 
