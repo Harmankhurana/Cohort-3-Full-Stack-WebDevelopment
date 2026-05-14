ddc7fe6fd9365ad6a8222f2a5be4687b4d535fd8
Harman khurana <work.harmankhurana@gmail.com>
2026-05-14 13:45:03 +0530

    feat: Fixing issue, bugs in the program and adding comments for better understanding

--- Week 10 - React.js Advance/SPAs-and-Routing/src/Router.jsx
+++ Week 10 - React.js Advance/SPAs-and-Routing/src/Router.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 
 // components from the react-router-dom library used for routing (navigation) in React apps.
-import {BrowserRouter, RouteS, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function ReactRouter() {
     return (
         <div>
             {/* making a BrowserRouter wrapper, we will wrap our application in BrowserRouter */}
             <BrowserRouter>
-                {/* here i will define all my routes */}
+                {/* here i will define all my routes, container for all routes */}
                 <Routes>
-                    <Route path = "/neet/online-coaching-class-11" element = { <class11program /> } />
-                    <Route path = "/neet/online-coaching-class-12" element = { <class12program /> } />
-                    <Route path = "/" element = { <landing /> } />
+                    {/* Route → defines path + component(element) and components are always capitalized */}
+                    <Route path = "/neet/online-coaching-class-11" element = { <Class11program /> } />
+                    <Route path = "/neet/online-coaching-class-12" element = { <Class12program /> } />
+                    <Route path = "/" element = { <Landing /> } />
                 </Routes>
             </BrowserRouter>
         </div>
@@ -20,21 +21,21 @@ function ReactRouter() {
 }
 
 // creating a function for class-11-program, calling it in ReactRouter (element)
-function class11program() {
+function Class11program() {
     return (
         <div>NEET programs for 11th class</div>
     )
 }
 
 // creating a function for class-12-program, calling it in ReactRouter (element)
-function class12program() {
+function Class12program() {
     return (
         <div>NEET programs for 12th class</div>
     )
 }
 
 // creating a function for landing page
-function landing() {
+function Landing() {
     return (
         <div>Welcome to Allen</div>
     )
