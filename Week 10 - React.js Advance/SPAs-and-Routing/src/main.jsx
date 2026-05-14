f4d27f35b1130a852979a0369cc8c7d3615f124a
Harman khurana <work.harmankhurana@gmail.com>
2026-05-14 16:04:20 +0530

    Importing RouterUseNavigator in main.jsx from RouterUseNavigator.jsx

--- Week 10 - React.js Advance/SPAs-and-Routing/src/main.jsx
+++ Week 10 - React.js Advance/SPAs-and-Routing/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import ReactRouter from './Router.jsx'
 import RouterLink from './RouterLink.jsx'
+import RouterUseNavigator from './RouterUseNavigate.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterLink />
+    <RouterUseNavigator />
   </StrictMode>,
 )
