2341ef9e85868874518f6ba887ceb6dd03aa2598
Harman khurana <work.harmankhurana@gmail.com>
2026-05-14 16:23:57 +0530

    feat: implement SPA routing with react-router-dom and add 404 fallback page

---
+++ Week 10 - React.js Advance/SPAs-and-Routing/src/RouterRandom.jsx
@@ -0,0 +1,73 @@
+// This SPA handles all unmatched routes in the application.
+// It renders a 404 (Page Not Found) error page when the user tries to access a route that does not exist.
+import React from "react";
+
+// components from the react-router-dom library used for routing (navigation) in React apps.
+import {BrowserRouter, Routes, Route, Link, useNavigate} from 'react-router-dom';
+
+function ReactRouter() {
+    return (
+        <div>
+            {/* making a BrowserRouter wrapper, we will wrap our application in BrowserRouter */}
+            <BrowserRouter>
+
+                {/* Better way of doing navigation rather than using anchor tag*/}
+                <Link to="/" >Allen</Link> | 
+                <Link to="/neet/online-coaching-class-11">Class 11</Link> | 
+                <Link to="/neet/online-coaching-class-12">Class 12</Link>
+
+                {/* here i will define all my routes, container for all routes */}
+                <Routes>
+                    {/* Route → defines path + component(element) and components are always capitalized */}
+                    <Route path = "/" element = { <Landing /> } />
+                    <Route path = "/neet/online-coaching-class-11" element = { <Class11program /> } />
+                    <Route path = "/neet/online-coaching-class-12" element = { <Class12program /> } />
+                    {/* to make an error page/ 404 page, I can use a star(*) in the path and the element which is having a component called ErrorPage */}
+                    <Route path = "*" element = { <ErrorPage /> } />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
+}
+
+// creating a function for landing page
+function Landing() {
+    return (
+        <div>Welcome to Allen</div>
+    )
+}
+
+// creating a function for class-11-program, calling it in ReactRouter (element)
+function Class11program() {
+    return (
+        <div>NEET programs for 11th class</div>
+    )
+}
+
+// creating a function for class-12-program, calling it in ReactRouter (element)
+function Class12program() {
+    // using useNavigate hook and putting it in a variable called navigate
+    const navigate = useNavigate();
+    
+    // a function which uses "navigate" variable to navigate back to home/landing page
+    function redirectUser() {
+        navigate("/");
+    }
+    
+    return (
+        <div>
+            <div>NEET programs for 12th class</div>
+            {/* a button which calls the redirectUser function */}
+            <button onClick={redirectUser}>Back to Landing Page</button>       
+        </div>
+    )
+}
+
+// Creating a function for navigating to error page/ 404 page
+function ErrorPage() {
+    return (
+        <div>Sorry page not found</div>
+    )
+}
+
+export default ReactRouter
