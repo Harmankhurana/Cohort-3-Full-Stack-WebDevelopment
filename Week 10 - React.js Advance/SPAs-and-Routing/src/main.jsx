8bb385b3090a6552bc6598f80ef5ff11dfc82ee8
Harman khurana <work.harmankhurana@gmail.com>
2026-05-14 16:24:59 +0530

    refactor: use RouterRandom as root component in main.jsx

--- Week 10 - React.js Advance/SPAs-and-Routing/src/main.jsx
+++ Week 10 - React.js Advance/SPAs-and-Routing/src/main.jsx
@@ -4,9 +4,10 @@ import App from './App.jsx'
 import ReactRouter from './Router.jsx'
 import RouterLink from './RouterLink.jsx'
 import RouterUseNavigator from './RouterUseNavigate.jsx'
+import RouterRandom from './RouterRandom.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterUseNavigator />
+    <RouterRandom />
   </StrictMode>,
 )
