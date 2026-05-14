5a8b7c1aaa4b31eb69c4e488e67201330eb5d17a
Harman khurana <work.harmankhurana@gmail.com>
2026-05-14 22:35:20 +0530

    Importing RouterLayout in main.jsx from RouterLayout.jsx

--- Week 10 - React.js Advance/SPAs-and-Routing/src/main.jsx
+++ Week 10 - React.js Advance/SPAs-and-Routing/src/main.jsx
@@ -5,9 +5,10 @@ import ReactRouter from './Router.jsx'
 import RouterLink from './RouterLink.jsx'
 import RouterUseNavigator from './RouterUseNavigate.jsx'
 import RouterRandom from './RouterRandom.jsx'
+import RouterLayout from './RouterLayout.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterRandom />
+    <RouterLayout />
   </StrictMode>,
 )
