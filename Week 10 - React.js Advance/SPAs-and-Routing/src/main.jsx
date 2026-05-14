78313446549f36bc470324d57086267047b47256
Harman khurana <work.harmankhurana@gmail.com>
2026-05-14 15:42:55 +0530

    Importing RouterLink in main.jsx from RouterLink.jsx

--- Week 10 - React.js Advance/SPAs-and-Routing/src/main.jsx
+++ Week 10 - React.js Advance/SPAs-and-Routing/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import ReactRouter from './Router.jsx'
+import RouterLink from './RouterLink.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ReactRouter />
+    <RouterLink />
   </StrictMode>,
 )
