ab516b1d7f2dff658736ed726ab819b74c86759d
Harman khurana <work.harmankhurana@gmail.com>
2026-05-15 16:01:00 +0530

    feat: implement clock component with start and stop functionality

---
+++ Week 10 - React.js Advance/SPAs-and-Routing/src/Clock.jsx
@@ -0,0 +1,12 @@
+// Creating a clock with a start and stop functionality
+import React from "react";
+
+function Clock() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Clock
