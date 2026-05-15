a21d32cd6d278574cc131488bf03a833e4295573
Harman khurana <work.harmankhurana@gmail.com>
2026-05-16 04:05:07 +0530

    feat: implement clock component with start and stop functionality

--- Week 10 - React.js Advance/SPAs-and-Routing/src/Clock.jsx
+++ Week 10 - React.js Advance/SPAs-and-Routing/src/Clock.jsx
@@ -4,9 +4,28 @@ import { useState } from "react";
 
 function Clock() {
     const [currentCount, setCurrentCount] = useState(0);
+    const [timer, setTimer] = useState(0);
+
+    // function for starting the clock
+    function startClock() {
+        let value = setInterval(() => {
+            setCurrentCount(currentCount => currentCount + 1);
+        }, 1000);
+        setTimer(value);
+    }
+
+    function stopClock() {
+        console.log(timerCount);
+        clearInterval(timerCount);
+    }
+
     return (
         <div>
             {currentCount}
+            <br />
+            <button onClick = {startClock} style={{cursor: 'pointer'}}>Start</button>
+            <button onClick  = {stopClock} style={{cursor: 'pointer'}}>Stop</button>
+            <button style={{cursor: 'pointer'}}>Clear</button>
         </div>
     )
 }
