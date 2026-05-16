55b92398504ac7e4b04523479220138651a498a7
Harman khurana <work.harmankhurana@gmail.com>
2026-05-16 13:44:48 +0530

    feat: Improving the code quality

--- Week 10 - React.js Advance/SPAs-and-Routing/src/Clock.jsx
+++ Week 10 - React.js Advance/SPAs-and-Routing/src/Clock.jsx
@@ -6,23 +6,26 @@ function Clock() {
     const [currentCount, setCurrentCount] = useState(0);
     // let timer = 0; -> not using this cause after every re-render the timer becomes 0
     // const [timer, setTimer] = useState(0); - > Even though useState persists values, using it for something like an interval ID is not ideal
-    const timer = useRef();
+    const timer = useRef(null);
 
     // function for starting the clock
     function startClock() {
-        let value = setInterval(() => {
+        if (timer.current) return;
+
+        timer.current = setInterval(() => {
             setCurrentCount(currentCount => currentCount + 1);
         }, 1000);
-        timer.current = value;
     }
 
     // function for stopping the clock
     function stopClock() {
         clearInterval(timer.current);
+        timer.current = null;
     }
 
     // function to clear the clock
     function clearClock() {
+        stopClock();
         setCurrentCount(0);
     }
 
