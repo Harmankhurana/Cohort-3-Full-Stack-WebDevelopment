7a4166c3a80d994d21736f7d66f6fcd2f3e72cac
Harman khurana <work.harmankhurana@gmail.com>
2026-05-23 01:55:47 +0530

    Importing LightBulbController from LightBulbDisplay.jsx in main.jsx

--- Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/main.jsx
+++ Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx';
 import Bulb from './LightBulb.jsx';
+import LightBulbController from './LightBulbDisplay.jsx.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Bulb />
+    <LightBulbController />
   </StrictMode>,
 )
