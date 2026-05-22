1ede31e0430e2d68a6564550e2966d10bd2326fb
Harman khurana <work.harmankhurana@gmail.com>
2026-05-23 01:52:38 +0530

    Add initial structure for LightBulb component with placeholder functions

--- Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/LightBulbImages.jsx
+++ Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/LightBulbImages.jsx
@@ -1,1 +1,20 @@
 // Creating a Light Bulb component -> which turns off/on when a button is clicked with added images
+import React from "react";
+
+function LightBulbController() {
+
+}
+
+function LightBulbWrapper() {
+
+}
+
+function LightBulbState() {
+
+}
+
+function handleBulbToggle() {
+
+}
+
+export default LightBulbController
