45c0464224bc750eb00052ce0462edc5307edb9f
Harman khurana <work.harmankhurana@gmail.com>
2026-05-23 02:08:00 +0530

    feat: integrate bulb images and implement toggle functionality

--- Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/LightBulbDisplay.jsx.jsx
+++ Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/LightBulbDisplay.jsx.jsx
@@ -1,6 +1,8 @@
 // Creating a Light Bulb component -> which turns off/on when a button is clicked with added images
 import React from "react";
 import { useState } from "react";
+import onBulb from './Images/onBulb.jpg';
+import offBulb from './Images/offBulb.png';
 
 function LightBulbController() {
     return (
@@ -31,8 +33,9 @@ function LightBulbState({bulbOn}) {
 
 function handleBulbToggle({setBulbOn}) {
     function Toggle() {
-        
+        setBulbOn(currentStatus = !currentStatus)
     }
+
     return (
         <div>
             <button onClick={Toggle}>Toggle Button</button>
