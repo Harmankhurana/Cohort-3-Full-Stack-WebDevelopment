4361b0959b9f9e7e9187508ff6f7e8268f7384c3
Harman khurana <work.harmankhurana@gmail.com>
2026-05-30 12:05:49 +0530

    Importing Prop drilling in main.jsx from PropDrilling.jsx

--- Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/main.jsx
+++ Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx';
 import Bulb from './LightBulb.jsx';
 import LightBulbController from './LightBulbDisplay.jsx.jsx';
+import PropDrilling from './PropDrilling.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LightBulbController />
+    <PropDrilling />
   </StrictMode>,
 )
