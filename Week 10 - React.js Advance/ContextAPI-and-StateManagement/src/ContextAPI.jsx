0c3b606643be9ab23ae1aa84552d968412580ed4
Harman khurana <work.harmankhurana@gmail.com>
2026-06-04 15:50:28 +0530

    feat: Implementing Context API workings on light bulb

--- Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/ContextAPI.jsx
+++ Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/ContextAPI.jsx
@@ -1,27 +1,29 @@
 // The Context API is a powerful feature in React that enables you to manage state across your application more effectively, especially when dealing with deeply nested components.
 // The Context API provides a way to share values (state, functions, etc.) between components without having to pass props down manually at every level.
 
-import React, { createContext } from "react";
+import React from "react";
 import { useState } from "react";
 import onBulb from './Images/onBulb.jpg';
 import offBulb from './Images/offBulb.png';
+import { createContext } from "react";
 
 // const bulbContext = React.createContext();
 // or - here createContext is directly taken from react
-const bulbContext = createContext();
+// I will store my light bulb props here(outside the main functions)
+const BulbContext = createContext();
 
 function App() {
-    // putting the state variable in the main app component
-    const [bulbOn, setBulbOn] = useState(true);
     return (
         <div>
-            {/* passing the props in light component */}
-            <Light bulbOn = {bulbOn} setBulbOn = {setBulbOn} />
+            {/* wrapping light component with BulbContext provider */}
+            <BulbContext.Provider>
+                <Light />
+            </BulbContext.Provider>
+            
         </div>
     )
 }
 
-// catching the props here and passing it to the children
 function Light() {
     return (
         <div>
