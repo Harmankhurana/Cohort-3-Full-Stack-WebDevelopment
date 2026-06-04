e98b22bdebe3a3444f61477ff82aaefb00c4f44e
Harman khurana <work.harmankhurana@gmail.com>
2026-06-05 04:15:43 +0530

    feat: Adding all the hooks in one container

--- Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/ContextAPI.jsx
+++ Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/ContextAPI.jsx
@@ -1,10 +1,8 @@
 // The Context API is a powerful feature in React that enables you to manage state across your application more effectively, especially when dealing with deeply nested components.
 // The Context API provides a way to share values (state, functions, etc.) between components without having to pass props down manually at every level.
-import React, { useContext } from "react";
-import { useState } from "react";
+import React, { useContext, useState, createContext } from "react";
 import onBulb from './Images/onBulb.jpg';
 import offBulb from './Images/offBulb.png';
-import { createContext } from "react";
 
 // const bulbContext = React.createContext();
 // or - here createContext is directly taken from react
