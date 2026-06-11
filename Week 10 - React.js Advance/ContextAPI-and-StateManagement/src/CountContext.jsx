779db9c7abf28f91bed2ed7848821b97f9b3f8f0
Harman khurana <work.harmankhurana@gmail.com>
2026-06-12 04:31:47 +0530

    feat: Creating a count component with context api for props passing

--- Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/CountContext.jsx
+++ Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/CountContext.jsx
@@ -1,30 +1,54 @@
 // Creating a count component with context api for props passing
-import React, { useState } from "react";
+import React, { createContext, useState } from "react";
+
+const CountContext = createContext();
 
 // parent component for Increase, Decrease and Value
-function CountContext() {
+function Parent() {
     const [count, setCount] = useState(0);
     return (
         <div>
-            <Increase />
-            <Decrease />
-            <Value />
+            <CountContext.Provider value={{count, setCount}}>
+                <Increase />
+                <Decrease />
+                <Value />
+            </CountContext.Provider>
         </div>
     )
 }
 
 // Component for Increasing the count
 function Increase() {
+    return (
+        <div>
 
+        </div>
+    )
 }
 
 // Component for Decreasing the count
 function Decrease() {
+    return (
+        <div>
 
+        </div>
+    )
 }
 
 function Value() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
 
+const CountContext = () => {
+    return (
+        <div>
+            <Parent />
+        </div>
+    )
 }
 
 export default CountContext
