196467540dcbf2de5acdafa83c89aa2e56d74f41
Harman khurana <work.harmankhurana@gmail.com>
2026-06-12 04:24:19 +0530

    feat: Creating the skeleton code for the count context component

--- Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/CountContext.jsx
+++ Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/CountContext.jsx
@@ -1,20 +1,30 @@
 // Creating a count component with context api for props passing
-import React from "react";
+import React, { useState } from "react";
 
+// parent component for Increase, Decrease and Value
 function CountContext() {
-
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            <Increase />
+            <Decrease />
+            <Value />
+        </div>
+    )
 }
 
+// Component for Increasing the count
 function Increase() {
 
 }
 
+// Component for Decreasing the count
 function Decrease() {
 
 }
 
 function Value() {
-    
+
 }
 
 export default CountContext
