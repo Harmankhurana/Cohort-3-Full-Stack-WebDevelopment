5039d3ed28b708e57912e44727da1ada5f23cbfb
Harman khurana <work.harmankhurana@gmail.com>
2026-06-12 14:31:58 +0530

    feat: Implement count state management using React Context API to avoid prop drilling

--- Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/CountContext.jsx
+++ Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/CountContext.jsx
@@ -1,44 +1,58 @@
 // Creating a count component with context api for props passing
-import React, { createContext, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 
-const CountContext = createContext();
+const CreateCountContext = createContext();
 
 // parent component for Increase, Decrease and Value
 function Parent() {
     const [count, setCount] = useState(0);
     return (
         <div>
-            <CountContext.Provider value={{count, setCount}}>
-                <Increase />
-                <Decrease />
-                <Value />
-            </CountContext.Provider>
+            <CreateCountContext.Provider value={{count, setCount}}>
+                <SubParent />
+            </CreateCountContext.Provider>
         </div>
     )
 }
 
-// Component for Increasing the count
-function Increase() {
+function SubParent() {
     return (
         <div>
-
+            <Increase />
+            <Decrease />
+            <Value />
+        </div>
+    )
+}
+// Component for Increasing the count
+function Increase() {
+    const {count, setCount} = useContext(CreateCountContext);
+    return (
+        <div>
+            <button onClick={() => setCount(count + 1)}>
+                Increase
+            </button>
         </div>
     )
 }
 
 // Component for Decreasing the count
 function Decrease() {
+    const {count, setCount} = useContext(CreateCountContext);
     return (
         <div>
-
+            <button onClick={() => setCount(count - 1)}>
+                Decrease
+            </button>
         </div>
     )
 }
 
 function Value() {
+    const {count} = useContext(CreateCountContext);
     return (
         <div>
-            
+            Count : {count}
         </div>
     )
 }
