c00d75e75552fb1fddece9afc75af89e722eecd1
Harman khurana <work.harmankhurana@gmail.com>
2026-06-13 17:42:00 +0530

    feat: initialize CountContext and component structure

--- Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/CountContextProvider.jsx
+++ Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/CountContextProvider.jsx
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { Children, createContext, useState } from "react";
+const CountContext = createContext();
+
+function CountContextProvider({Children}) {
+    const[count, setCount] = useState(0);
+    return (
+        <div>
+            <CountContext.Provider value={{count, setCount}}>
+                {Children}
+            </CountContext.Provider>
+        </div>
+    )
+}
 
 function Parent() {
-
+    return (
+        <div>
+            <CountContextProvider>
+                <Increase />
+                <Decrease />
+                <Value />
+            </CountContextProvider>
+        </div>
+    )
 }
 
 function Increase() {
+    return (
+        <div>
 
+        </div>
+    )
 }
 
 function Decrease() {
+    return (
+        <div>
 
+        </div>
+    )
 }
 
 function Value() {
+    return (
+        <div>
 
+        </div>
+    )
 }
 
 export default Parent
