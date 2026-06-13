a9c391aa3c5fb08a17bd30b8e8b8bc13db217590
Harman khurana <work.harmankhurana@gmail.com>
2026-06-13 16:53:33 +0530

    feat: Creating a bulb provider wrapper and passing the parent as child in it

---
+++ Week 10 - React.js Advance/ContextAPI-and-StateManagement/src/BulbProvider.jsx
@@ -0,0 +1,69 @@
+// Here i'm creating a bulb Provider wrapper and taking <Light /> as a child in it
+import React, { useContext, useState, createContext } from "react";
+import onBulb from './Images/onBulb.jpg';
+import offBulb from './Images/offBulb.png';
+
+// const bulbContext = React.createContext();
+// or - here createContext is directly taken from react
+// I will store my light bulb props here(outside the main functions)
+const BulbContext = createContext();
+
+// Creating the Provider wrapper and passing light as children in it
+function BulbProvider({children}) {
+    const [bulbOn, setBulbOn] = useState(true);
+    return (
+        <div>
+            {/* wrapping light component with BulbContext provider */}
+            <BulbContext.Provider value={{bulbOn, setBulbOn}}>
+                {children}
+            </BulbContext.Provider>
+
+        </div>
+    )
+}
+
+// the main component (ContextAPI) which is wrapped by BulbProvider
+function BulbContextProvider() {
+    return (
+        <div>
+            <BulbProvider>
+                <Light />
+            </BulbProvider>
+        </div>
+    )
+}
+
+function Light() {
+    return (
+        <div>
+            <LightBulb />
+            <LightSwitch />
+        </div>
+    )
+}
+
+// passing props directly from context rather than doing prop drilling
+function LightBulb() {
+    const {bulbOn} = useContext(BulbContext);
+    return (
+        <div>
+            {bulbOn ? <img src = {onBulb} /> : <img src = {offBulb} height="350px"/>}
+        </div>
+    )
+}
+
+// passing props directly from context rather than doing prop drilling
+function LightSwitch() {
+    const {setBulbOn} = useContext(BulbContext);
+    function Toggle() {
+        setBulbOn(currentStatus => !currentStatus)
+    }
+
+    return (
+        <div>
+            <button onClick={Toggle}>Toggle Button</button>
+        </div>
+    )
+}
+
+export default BulbContextProvider
