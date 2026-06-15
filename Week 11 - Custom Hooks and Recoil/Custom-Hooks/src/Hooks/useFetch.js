78e862c9784d0034b40e704d5ece897b4a6a6afb
Harman khurana <work.harmankhurana@gmail.com>
2026-06-15 22:48:55 +0530

    feat: Implementing setInterval which sends the request to the server after every set intervals of time

--- Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/Hooks/useFetch.js
+++ Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/Hooks/useFetch.js
@@ -17,7 +17,18 @@ export function useFetch(url) {
     }
     useEffect(() => {
         getDetails();
-    }, [url]) // this says that whenever the url changes get the details back from the server
+    }, [url]) // this says that whenever the url changes get the details back from the server(dynamic url)
+
+    // this hook will reassure that afte every 10 sec a request should be sent to the backend server
+    useEffect(() => {
+        const interval = setInterval(() => {
+            getDetails();
+        }, 10000);
+
+        return () => clearInterval(interval);
+    }, [url]); // url here matters as a dependency cause if i don't write it
+    // interval runs with the initial url only
+    // If url changes → interval still uses old one
 
     return {
         finalData,
