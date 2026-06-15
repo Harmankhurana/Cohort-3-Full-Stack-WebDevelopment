9cc26851d50228d7cfb063ccae493d1ea7834d1e
Harman khurana <work.harmankhurana@gmail.com>
2026-06-15 17:46:11 +0530

    feat: Creating a AdvUseFetch component which is more generic, uses url as an argument and fetch data

--- Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/AdvUseFetch.jsx
+++ Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/AdvUseFetch.jsx
@@ -1,14 +1,14 @@
 // A better way is to create a custom hook(useFetch.js) and use it here
 // This way the code is not cluttered as the fetching logic is in a seperate file and here I just have to import it
-import React from "react";
-// importing usePostTitle custom hook
-import { usePostTitle } from "./Hooks/useFetch";
+import React, { use } from "react";
+// importing useFetch custom hook
+import { useFetch } from "./Hooks/useFetch";
 
 function AdvUseFetch() {
-    const postTitle = usePostTitle();
+    const finalData = useFetch("https://jsonplaceholder.typicode.com/posts/1");
     return (
         <div>
-            {postTitle}
+            {JSON.stringify(finalData)}
         </div>
     )
 }
