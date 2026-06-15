a339fad7906c6233c5b18a6c7f0984d0fd354ae3
Harman khurana <work.harmankhurana@gmail.com>
2026-06-15 20:04:45 +0530

    Implementing loading logic in the useFetch custom hook

--- Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/Hooks/useFetch.js
+++ Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/Hooks/useFetch.js
@@ -6,17 +6,21 @@ import { useEffect, useState } from "react";
 
 export function useFetch(url) {
     const [finalData, setFinalData] = useState({});
+    const [loading, setLoading] = useState(true);
 
     async function getDetails() {
+        setLoading(true);
         const response = await fetch(url);
         const json = await response.json();
         setFinalData(json);
+        setLoading(false);
     }
     useEffect(() => {
         getDetails();
-    }, [])
+    }, [url]) // this says that whenever the url changes get the details back from the server
 
     return {
-        finalData
+        finalData,
+        loading
     }
 };
