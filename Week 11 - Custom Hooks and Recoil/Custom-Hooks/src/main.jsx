f17050ed8ed373dc333c5a8978a5084814d2930f
Harman khurana <work.harmankhurana@gmail.com>
2026-06-15 17:47:58 +0530

    Importing UsePostTitle in main.jsx from UsePostTitle.jsx

--- Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/main.jsx
+++ Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import UseCounter from './UseCounter.jsx'
 import UseFetch from './UseFetch.jsx'
+import UsePostTitle from './UsePostTitle.jsx'
 import AdvUseFetch from './AdvUseFetch.jsx'
 
 createRoot(document.getElementById('root')).render(
