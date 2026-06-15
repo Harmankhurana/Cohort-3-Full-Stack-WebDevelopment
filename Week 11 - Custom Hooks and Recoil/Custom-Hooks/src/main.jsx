84357738b1647f94b20f02911babf8d08bdcd25d
Harman khurana <work.harmankhurana@gmail.com>
2026-06-15 20:05:34 +0530

    Importing MoreAdvUseFetch in main.jsx from MoreAdvUseFetch.jsx

--- Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/main.jsx
+++ Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/main.jsx
@@ -5,9 +5,10 @@ import UseCounter from './UseCounter.jsx'
 import UseFetch from './UseFetch.jsx'
 import UsePostTitle from './UsePostTitle.jsx'
 import AdvUseFetch from './AdvUseFetch.jsx'
+import MoreAdvUseFetch from './MoreAdvUseFetch.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AdvUseFetch />
+    <MoreAdvUseFetch />
   </StrictMode>,
 )
