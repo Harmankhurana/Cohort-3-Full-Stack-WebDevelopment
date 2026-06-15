b7abbaf095b9fdb09a74cbd929f938ed9ec58c01
Harman khurana <work.harmankhurana@gmail.com>
2026-06-15 20:13:41 +0530

    feat: implement dynamic API fetching with loading state and post switching

--- Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/MoreAdvUseFetch.jsx
+++ Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/MoreAdvUseFetch.jsx
@@ -3,15 +3,30 @@ import { useFetch } from "./Hooks/useFetch";
 
 function MoreAdvUseFetch() {
     const [currentPost, setCurrentPost] = useState(1);
-    const { finalData } = useFetch("https://jsonplaceholder.typicode.com/posts/" + currentPost);
+    const { finalData, loading } = useFetch("https://jsonplaceholder.typicode.com/posts/" + currentPost);
+
+    if ( loading ) {
+        return (
+            <div>
+                Loading...
+            </div>
+        )
+    }
 
     return (
         <div>
-            <div><button onClick={() => setCurrentPost(1)}>Post 1</button></div>
-            <div><button onClick={() => setCurrentPost(2)}>Post 2</button></div>
-            <div><button onClick={() => setCurrentPost(3)}>Post 3</button></div>
-
-            {JSON.stringify(finalData)}
+            <div>
+                <button onClick={() => setCurrentPost(1)}>Post 1</button>
+                <button onClick={() => setCurrentPost(2)}>Post 2</button>
+                <button onClick={() => setCurrentPost(3)}>Post 3</button>
+                <button onClick={() => setCurrentPost(4)}>Post 4</button>
+                <button onClick={() => setCurrentPost(5)}>Post 5</button>
+                <button onClick={() => setCurrentPost(6)}>Post 6</button>
+            </div>
+            
+            <div>
+                {JSON.stringify(finalData)}
+            </div>
         </div>
     )
 }
