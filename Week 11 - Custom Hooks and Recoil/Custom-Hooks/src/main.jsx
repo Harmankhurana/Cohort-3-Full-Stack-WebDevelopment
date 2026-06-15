18dc01167ca89e049af710ddb45e0548c2780b87
Harman khurana <work.harmankhurana@gmail.com>
2026-06-15 10:59:53 +0530

    Importing AdvUseFetch in main.jsx from AdvUseFetch.jsx

--- Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/main.jsx
+++ Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import UseCounter from './UseCounter.jsx'
 import UseFetch from './UseFetch.jsx'
+import AdvUseFetch from './AdvUseFetch.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UseFetch />
+    <AdvUseFetch />
   </StrictMode>,
 )
