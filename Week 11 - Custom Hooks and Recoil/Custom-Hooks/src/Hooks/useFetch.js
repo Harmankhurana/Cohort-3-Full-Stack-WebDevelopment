99c630a8f91261119dc3278dd2fbdeea347c76b1
Harman khurana <work.harmankhurana@gmail.com>
2026-06-15 17:47:13 +0530

    Creating a generic useFetch hook, that takes url as an argument and returns the data

--- Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/Hooks/useFetch.js
+++ Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/Hooks/useFetch.js
@@ -1,34 +1,22 @@
-import { useEffect, useState } from "react";
-
-// A custom Hook that hits the backend of the API to fetch the data
-export function usePostTitle() {
-    const [post, setPost] = useState({});
-
-    async function getPosts() {
-        const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
-        const json = await response.json();
-        setPost(json);
-    }
-
-    useEffect(() => {
-        getPosts();
-    }, []);
-
-    return post.title;
-}
-
 // Creating a generic useFetch hook, that takes url as an argument and returns the data
 // Hits the server and get response
 // URL --> useFetch Hook --> returns response/data
 
-export function useFetch() {
+import { useEffect, useState } from "react";
+
+export function useFetch(url) {
     const [finalData, setFinalData] = useState({});
 
-    function getDetails() {
-        
+    async function getDetails() {
+        const response = await fetch(url);
+        const json = response.json();
+        setFinalData(json);
     }
-
     useEffect(() => {
         getDetails();
     }, [])
-}
+
+    return {
+        finalData
+    }
+};
