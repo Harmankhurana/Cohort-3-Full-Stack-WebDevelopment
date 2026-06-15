e76b12791696d54b36c658cc288451c927b02d0c
Harman khurana <work.harmankhurana@gmail.com>
2026-06-15 09:46:26 +0530

    Importing UseFetch in main.jsx from UseFetch.jsx

--- Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/main.jsx
+++ Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import UseCounter from './UseCounter.jsx'
+import UseFetch from './UseFetch.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UseCounter />
+    <UseFetch />
   </StrictMode>,
 )
