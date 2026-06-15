e7379171d3973cc349c0f5c1b482325388eb1b9f
Harman khurana <work.harmankhurana@gmail.com>
2026-06-15 17:42:26 +0530

    feat: Creating a UsePostTitle component which uses a custom hook to fetch the data and render it

---
+++ Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/UsePostTitle.jsx
@@ -0,0 +1,16 @@
+// A better way is to create a custom hook(usePostTitle.js) and use it here
+// This way the code is not cluttered as the fetching logic is in a seperate file and here I just have to import it
+import React from "react";
+// importing usePostTitle custom hook
+import { usePostTitle } from "./Hooks/usePostTitle";
+
+function UsePostTitle() {
+    const postTitle = usePostTitle();
+    return (
+        <div>
+            {postTitle}
+        </div>
+    )
+}
+
+export default UsePostTitle
