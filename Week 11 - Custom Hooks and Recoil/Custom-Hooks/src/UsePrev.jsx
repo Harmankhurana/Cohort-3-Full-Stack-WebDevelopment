85fbcb5eb893f3e36824381c52c0eb9b163b3794
Harman khurana <work.harmankhurana@gmail.com>
2026-06-21 10:23:42 +0530

    feat: implement usePrev hook to track previous state in UsePrev component

---
+++ Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/UsePrev.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { usePrev } from "./Hooks/usePrev";
+
+function UsePrev() {
+    const [state, setState] = useState(0);
+    const prev = usePrev(state);
+
+    return (
+        <div>
+            {state}
+            <button onClick={() => {setState((curr) => curr + 1)}}>Click Here</button>
+            <h1>This is the Prev value {state}</h1>
+        </div>
+    )
+}
+
+export default UsePrev
