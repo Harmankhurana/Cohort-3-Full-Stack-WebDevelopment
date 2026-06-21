3ad02e47e38c39fa5527da33ff8f8f71d5477e27
Harman khurana <work.harmankhurana@gmail.com>
2026-06-21 23:16:40 +0530

    Adding logs to check the exact values in inspect mode

--- Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/Hooks/usePrev.js
+++ Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/Hooks/usePrev.js
@@ -2,12 +2,15 @@ import { useEffect, useRef, useState } from 'react';
 
 export function usePrev (value) {
     const ref = useRef();
+    console.log(`Re-render happend with a new value ${value}`);
 
     useEffect(() => {
+        console.log(`Update the ref to be ${value}`);
         ref.current = value;
     },[value]);
 
-    return ref.current
+    console.log(`returned ${ref.current}`);
+    return ref.current; // return will print the prev value first then the useEffect will trigger
 }
 
 // this hooks returns first then effects get called later
