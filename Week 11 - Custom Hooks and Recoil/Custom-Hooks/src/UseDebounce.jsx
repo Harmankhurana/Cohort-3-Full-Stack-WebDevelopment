4b48a35ffaf6a11dba8a7341136f2de38d3a77ca
Harman khurana <work.harmankhurana@gmail.com>
2026-06-24 10:21:09 +0530

    feat: Creating a UseDebounce component which hits the backend after a specific time delay

---
+++ Week 11 - Custom Hooks and Recoil/Custom-Hooks/src/UseDebounce.jsx
@@ -0,0 +1,30 @@
+// Debounce Function
+// This function ensures that a given function (e.g., search API call)
+// is only executed after the user has stopped triggering it for a specified delay.
+// Use case:
+// - Prevents multiple unnecessary API calls while the user is typing
+// - Improves performance and reduces backend load
+//
+// How it works:
+// - Every time the function is called, the previous timer is cleared
+// - A new timer starts
+// - The actual function executes only after the delay has passed
+//   without any new calls
+//
+// Example:
+// User types "hello"
+// -> h (wait)
+// -> he (reset timer)
+// -> hel (reset timer)
+// -> hell (reset timer)
+// -> hello (wait 500ms)
+// => API call happens once with "hello"
+// but here i will call a custom hook called "useBounce" to do the task
+
+import React from "react";
+
+function UseDebounce() {
+
+}
+
+export default UseDebounce
