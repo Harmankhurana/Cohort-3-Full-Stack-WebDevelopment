3a83e1d0afb6948351e7cdd5623923c052ef701e
Harman khurana <work.harmankhurana@gmail.com>
2026-06-26 22:12:46 +0530

    Writing Comments for better understanding of the code base

--- Week 11 - Custom Hooks and Recoil/Recoil/src/RecoilCounterApplication.jsx
+++ Week 11 - Custom Hooks and Recoil/Recoil/src/RecoilCounterApplication.jsx
@@ -3,6 +3,9 @@ import React from "react";
 import { RecoilRoot, atom, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { counterAtom } from "./store/atoms/counter";
 
+// Counter app using Recoil for global state management
+// RecoilRoot provides a shared state container for all child components
+// RecoilRoot wraps the app to provide access to Recoil state
 function RecoilCounterApplication() {
     return (
         <div>
@@ -13,6 +16,7 @@ function RecoilCounterApplication() {
     )
 }
 
+// Main component that groups all counter-related UI elements
 function Counter() {
     return (
         <div>
@@ -23,6 +27,8 @@ function Counter() {
     )
 }
 
+// This component reads the current value of the counter atom
+// useRecoilValue subscribes to the atom and re-renders only when its value changes
 function CurrentCount() {
     const count = useRecoilValue(counterAtom);
     return (
@@ -32,10 +38,14 @@ function CurrentCount() {
     )
 }
 
+
+// This component updates (increments) the counter value
+// useSetRecoilState provides a setter function without subscribing to state changes
 function Increase() {
     const setCount = useSetRecoilState(counterAtom);
 
     function increase() {
+        // Functional update ensures we always get the latest state value
         setCount(c => c + 1)
     }
     return (
@@ -45,10 +55,13 @@ function Increase() {
     )
 }
 
+// This component updates (decrements) the counter value
+// Similar to Increase, but decreases the value instead
 function Decrease() {
     const setCount = useSetRecoilState(counterAtom);
 
     function decrease() {
+        // Functional update ensures we always get the latest state value
         setCount(c => c - 1)
     }
 
