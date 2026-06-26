e744811d6b259b429ea856d8f851d48286c9aff3
Harman khurana <work.harmankhurana@gmail.com>
2026-06-26 11:51:41 +0530

    feat: create global counter state using Recoil atom

---
+++ Week 11 - Custom Hooks and Recoil/Recoil/src/store/atoms/counter.js
@@ -0,0 +1,9 @@
+// Defining a Recoil atom to manage counter state globally across components
+// key: unique ID required by Recoil to identify this atom
+// default: sets the initial value of the counter to 0
+import { atom } from 'recoil';
+
+export const CounterAtom = atom({
+    key: "counter", 
+    default: 0
+})
