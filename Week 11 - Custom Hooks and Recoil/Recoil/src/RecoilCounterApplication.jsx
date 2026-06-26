1f443f23ff4c2c9723c01839c2264f8ad701b4b9
Harman khurana <work.harmankhurana@gmail.com>
2026-06-26 11:50:14 +0530

    feat: implement counter UI structure with CurrentCount, Increase, and Decrease components

--- Week 11 - Custom Hooks and Recoil/Recoil/src/RecoilCounterApplication.jsx
+++ Week 11 - Custom Hooks and Recoil/Recoil/src/RecoilCounterApplication.jsx
@@ -1,6 +1,7 @@
 // Creating a Counter Application using recoil for better state management
 import React from "react";
-import { RecoilRoot } from 'recoil';
+import { RecoilRoot, atom } from 'recoil';
+import { CounterAtom } from "./store/atoms/counter";
 
 function RecoilCounterApplication() {
     return (
@@ -15,7 +16,33 @@ function RecoilCounterApplication() {
 function Counter() {
     return (
         <div>
-            
+            <CurrentCount />
+            <Increase />
+            <Decrease />
+        </div>
+    )
+}
+
+function CurrentCount() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+function Increase() {
+    return (
+        <div>
+            <button>Increment</button>
+        </div>
+    )
+}
+
+function Decrease() {
+    return (
+        <div>
+            <button>Decrement</button>
         </div>
     )
 }
