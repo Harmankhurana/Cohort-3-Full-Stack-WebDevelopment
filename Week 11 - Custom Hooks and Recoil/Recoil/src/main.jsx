6c4fc53cd44a9d52869e2d174aa8724334dbc8a6
Harman khurana <work.harmankhurana@gmail.com>
2026-06-26 11:36:04 +0530

    Importing RecoilCounterApplication in main.jsx from RecoilCounterApplication.jsx

--- Week 11 - Custom Hooks and Recoil/Recoil/src/main.jsx
+++ Week 11 - Custom Hooks and Recoil/Recoil/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ContextAPICounterApplication from './ContextAPICounterApplication.jsx'
+import RecoilCounterApplication from './RecoilCounterApplication.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ContextAPICounterApplication />
+    <RecoilCounterApplication />
   </StrictMode>,
 )
