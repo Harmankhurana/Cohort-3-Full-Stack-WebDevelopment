431387a87ba25ca3b29e9e8fd1cf1adf6265f8a9
Harman khurana <work.harmankhurana@gmail.com>
2026-07-01 17:36:13 +0530

    feat: Creating a Selector counter application

---
+++ Week 11 - Custom Hooks and Recoil/Recoil/src/SelectorCounterApplication.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+function SelectorCounterApplication() {
+    return (
+        <div>
+            <Buttons />
+            <Counter />
+            <isEven />
+        </div>
+    )
+}
+
+function Buttons() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+function isEven() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+function isOdd() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SelectorCounterApplication
