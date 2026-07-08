c7c934a89e9584c01e4c2c39fb0104179bfe1a45
Harman khurana <work.harmankhurana@gmail.com>
2026-07-09 01:52:40 +0530

    feat: Wrapping Navbar component in RecoilRoot and importing the atoms

--- Week 11 - Custom Hooks and Recoil/Recoil-Deep-Dive/src/RecoilNavbar.jsx
+++ Week 11 - Custom Hooks and Recoil/Recoil-Deep-Dive/src/RecoilNavbar.jsx
@@ -1,12 +1,28 @@
 import React from "react";
-import { networkAtom, jobsAtom, messagesAtom, notificationAtom } from "./ReacoilNavbarAtom";
+import { networkAtom, jobsAtom, messagesAtom, notificationAtom } from "./ReacoilNavbarAtom.js";
+import { RecoilRoot, useRecoilValue } from "recoil";
+
+function RecoilNavbar() {
+    return (
+        <RecoilRoot>
+            <div>
+                <Navbar />
+            </div>            
+        </RecoilRoot>
+
+    )
+}
 
 function Navbar() {
+    const networkNotificationCount = useRecoilValue(networkAtom);
+    const jobsAtomCount = useRecoilValue(jobsAtom);
+    const messagesAtomCount = useRecoilValue(messagesAtom);
+    const notificationAtomCount = useRecoilValue(notificationAtom);
     return (
         <div>
             <button>Home</button>
-            <button>My Network</button>
-            <button>Jobs</button>
+            <button>My Network ({networkNotificationCount >= 100 ? "99+" : networkNotificationCount})</button> {/*if networkNotificationCount is greater than equal to 100 then show 99+ or show the actual value*/}
+            <button>Jobs ({jobsNotificationCount >= 100 ? "99+" : jobsNotificationCount})</button>
             <button>Messages</button>
             <button>Notification</button>
             <button>Me</button>
@@ -14,4 +30,4 @@ function Navbar() {
     )
 }
 
-export default Navbar
+export default RecoilNavbar
