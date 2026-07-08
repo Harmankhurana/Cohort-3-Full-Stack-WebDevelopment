777cc0a03cf93c38168c941eafda884ba14011e5
Harman khurana <work.harmankhurana@gmail.com>
2026-07-08 18:37:48 +0530

    Importing RecoilNavbar in main.jsx from RecoilNavbar.jsx

--- Week 11 - Custom Hooks and Recoil/Recoil-Deep-Dive/src/main.jsx
+++ Week 11 - Custom Hooks and Recoil/Recoil-Deep-Dive/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import Navbar from './Navbar.jsx'
+import RecoilNavbar from './RecoilNavbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navbar />
+    <RecoilNavbar />
   </StrictMode>,
 )
