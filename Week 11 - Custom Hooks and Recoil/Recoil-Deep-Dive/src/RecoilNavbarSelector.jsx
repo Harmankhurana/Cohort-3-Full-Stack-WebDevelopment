65cd91675af66f542905c09b6ffd1ca5582b7b39
Harman khurana <work.harmankhurana@gmail.com>
2026-07-09 02:20:58 +0530

    feat: Creating a Recoil Navbar Selector application

---
+++ Week 11 - Custom Hooks and Recoil/Recoil-Deep-Dive/src/RecoilNavbarSelector.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function RecoilNavbarSelector() {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
+
+function Navbar() {
+    const networkNotificationCount = useRecoilValue(networkAtom);
+    const jobsAtomCount = useRecoilValue(jobsAtom);
+    const messagesAtomCount = useRecoilValue(messagesAtom);
+    const notificationAtomCount = useRecoilValue(notificationAtom);
+    return (
+        <div>
+            <button>Home</button>
+            <button>My Network ({networkNotificationCount >= 100 ? "99+" : networkNotificationCount})</button> {/*if networkNotificationCount is greater than equal to 100 then show 99+ or show the actual value*/}
+            <button>Jobs ({jobsAtomCount >= 100 ? "99+" : jobsAtomCount})</button>
+            <button>Messages ({messagesAtomCount >= 100 ? "99+" : messagesAtomCount})</button>
+            <button>Notification ({notificationAtomCount >= 100 ? "99+" : notificationAtomCount})</button>
+            <button>Me</button>
+        </div>
+    )
+}
+
+export default RecoilNavbarSelector
