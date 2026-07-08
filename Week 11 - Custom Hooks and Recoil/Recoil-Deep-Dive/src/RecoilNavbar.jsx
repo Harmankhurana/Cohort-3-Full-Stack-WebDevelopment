d6c07347e418dca6cd984dda56ceafeab2971be9
Harman khurana <work.harmankhurana@gmail.com>
2026-07-08 18:37:10 +0530

    feat: Creating Recoil Navbar Component

---
+++ Week 11 - Custom Hooks and Recoil/Recoil-Deep-Dive/src/RecoilNavbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div>
+            <button>Home</button>
+            <button>My Network</button>
+            <button>Jobs</button>
+            <button>Messages</button>
+            <button>Notification</button>
+            <button>Me</button>
+        </div>
+    )
+}
+
+export default Navbar
