b13d8854cfcd17369201bb8d272bf717affd993e
Harman khurana <work.harmankhurana@gmail.com>
2026-07-09 02:21:59 +0530

    Fixing bugs and typo errors

--- Week 11 - Custom Hooks and Recoil/Recoil-Deep-Dive/src/RecoilNavbarSelector.jsx
+++ Week 11 - Custom Hooks and Recoil/Recoil-Deep-Dive/src/RecoilNavbarSelector.jsx
@@ -9,14 +9,14 @@ function RecoilNavbarSelector() {
 }
 
 function Navbar() {
-    const networkNotificationCount = useRecoilValue(networkAtom);
+    const networkAtomCount = useRecoilValue(networkAtom);
     const jobsAtomCount = useRecoilValue(jobsAtom);
     const messagesAtomCount = useRecoilValue(messagesAtom);
     const notificationAtomCount = useRecoilValue(notificationAtom);
     return (
         <div>
             <button>Home</button>
-            <button>My Network ({networkNotificationCount >= 100 ? "99+" : networkNotificationCount})</button> {/*if networkNotificationCount is greater than equal to 100 then show 99+ or show the actual value*/}
+            <button>My Network ({networkAtomCount >= 100 ? "99+" : networkAtomCount})</button> {/*if networkNotificationCount is greater than equal to 100 then show 99+ or show the actual value*/}
             <button>Jobs ({jobsAtomCount >= 100 ? "99+" : jobsAtomCount})</button>
             <button>Messages ({messagesAtomCount >= 100 ? "99+" : messagesAtomCount})</button>
             <button>Notification ({notificationAtomCount >= 100 ? "99+" : notificationAtomCount})</button>
