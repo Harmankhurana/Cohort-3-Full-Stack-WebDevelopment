3ec1ef682d679f2985ba45abcfbdd80c8396bc8b
Harman khurana <work.harmankhurana@gmail.com>
2026-07-12 00:53:44 +0530

    Importing AxiosFetch in main.jsx from AxiosFetch.jsx

--- Week 11 - Custom Hooks and Recoil/Async-Queries/src/main.jsx
+++ Week 11 - Custom Hooks and Recoil/Async-Queries/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import AxiosFetch from './Axios.Fetch.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AxiosFetch />
   </StrictMode>,
 )
