3ed4e77964bb07f656c686370b80b52340d8db84
Harman khurana <work.harmankhurana@gmail.com>
2026-07-16 17:16:37 +0530

    Returning true or false based on if a user is 18+

--- Week 14 - TypeScript/TypeScript Part - 1/index.ts
+++ Week 14 - TypeScript/TypeScript Part - 1/index.ts
@@ -37,3 +37,16 @@ function sum(a: number, b: number) {
 }
 
 console.log(sum(3,6));
+
+
+
+// Return true or false based on if a user is 18+
+function isLegal(age: number) {
+    if (age > 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(isLegal(19));
