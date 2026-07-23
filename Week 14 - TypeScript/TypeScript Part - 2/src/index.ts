585058339d8d9e5aa404bf2a691e971f82633c16
Harman khurana <work.harmankhurana@gmail.com>
2026-07-23 16:39:42 +0530

    feat: Creating a function which returns true or false depending upon the number is even or not

--- Week 14 - TypeScript/TypeScript Part - 2/src/index.ts
+++ Week 14 - TypeScript/TypeScript Part - 2/src/index.ts
@@ -1,2 +1,11 @@
-let name: string = "Harman Singh Khurana";
-console.log(name);
+// A function which returns true or false depending upon the number is even or not
+function isEven(num: number): boolean {
+    if(num % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(isEven(4)); // true -> even
+console.log(isEven(9)); // false -> odds
