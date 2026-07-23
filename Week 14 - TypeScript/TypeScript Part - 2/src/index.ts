649aaac5145c01d02137a9827546a538618e6a1a
Harman khurana <work.harmankhurana@gmail.com>
2026-07-24 00:46:23 +0530

    feat: Creating a function called isLegal and logging it's values

--- Week 14 - TypeScript/TypeScript Part - 2/src/index.ts
+++ Week 14 - TypeScript/TypeScript Part - 2/src/index.ts
@@ -15,7 +15,7 @@ console.log(isEven(9)); // false -> odds
 
 
 // defining an interface to enforce type structure for a complex object
-interface User {
+interface UserInterface {
     name: string,
     age: number,
     address: {
@@ -28,7 +28,7 @@ interface User {
 }
 
 // creating a complex object
-let user = {
+let User: UserInterface = {
     name: "Harman",
     age: 21,
     address: {
@@ -38,4 +38,21 @@ let user = {
         pincode: 313001
     },
     mobileNo: "+91 0000000000"
+}
+
+// using the object in isLegal or not function
+function isLegal(User: UserInterface): boolean {
+    if(User.age > 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// logging ans
+const ans = isLegal(User);
+if (ans) {
+    console.log("I'm legal");
+} else {
+    console.log("I'm illegal");
 }
