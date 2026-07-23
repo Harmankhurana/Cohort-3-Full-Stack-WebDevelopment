4a459ccfdca51d8028237ef708e9b101a191167d
Harman khurana <work.harmankhurana@gmail.com>
2026-07-23 20:25:51 +0530

    feat: defining an interface to enforce type structure for a complex object

--- Week 14 - TypeScript/TypeScript Part - 2/src/index.ts
+++ Week 14 - TypeScript/TypeScript Part - 2/src/index.ts
@@ -9,3 +9,33 @@ function isEven(num: number): boolean {
 
 console.log(isEven(4)); // true -> even
 console.log(isEven(9)); // false -> odds
+
+
+
+
+
+// defining an interface - which will help my complex object to have types
+interface User {
+    name: string,
+    age: number,
+    address: {
+        city: string,
+        state: string,
+        country: string,
+        pincode: number
+    },
+    mobileNo: string
+}
+
+// creating a complex object
+let user = {
+    name: "Harman",
+    age: 21,
+    address: {
+        city: "Udaipur",
+        state: "Rajasthan",
+        country: "India",
+        pincode: 313001
+    },
+    mobileNo: "+91 0000000000"
+}
